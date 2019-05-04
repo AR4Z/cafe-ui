cbff5cb60ac273473b9973bf40e4536ecc04c51c
Orlando Hernandez <oaraz02@gmail.com>
2019-05-04 08:59:47 -0500

    change url

--- src/views/StaffAssignment.vue
+++ src/views/StaffAssignment.vue
@@ -226,7 +226,7 @@ export default {
             kgs.push(lote.kg);
           }
           this.axios
-            .post("coffee-assignment.herokuapp.com/v1/schedule", {
+            .post("http://coffee-assignment.herokuapp.com/v1/schedule", {
               rendimientos: rendimientos,
               pendientes: pendientes,
               kgs: kgs
@@ -248,7 +248,7 @@ export default {
     },
     checkScheduleStatus() {
       this.axios
-        .get(`coffee-assignment.herokuapp.com/v1/schedule/${this.token}`)
+        .get(`http://coffee-assignment.herokuapp.com/v1/schedule/${this.token}`)
         .then(res => {
           if (res.data.status == "SUCCESS") {
             clearInterval(this.refreshId);
